76d4ac61794d966bd9543c02e6ecbec9f09594e8
sebastianarias16 <sebas.game.am12@gmail.com>
2020-08-16 18:34:55 -0500

    ejercicios

---
+++ clase 1/ejercicio 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text; 
+using System.Collections;  
+using System.Collections.Generic;
+
+namespace ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k, l;
+            Console.Write("ingrese cantidad de k: ");
+            k = double.Parse(Console.ReadLine());
+            l = 0.453592;
+            l = k * l;
+            Console.WriteLine("equivale en l {0}", l);
+            Console.ReadLine();
+        }
+    }
+}
